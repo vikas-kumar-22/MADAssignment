86cabdeecdcf82c7b4ad5b345c6ed70b70f9fff0
vikas-kumar-22 <vikas86.6kumar@gmail.com>
2025-04-11 14:52:44 +0530

    done question 3

---
+++ Ques3/app/src/main/java/com/example/ques3/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.ques3; // use your actual package name
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
